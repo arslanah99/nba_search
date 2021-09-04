d2b0f47e57129be1d56e4b92a1e1f65123145738
CHINEDU GODWIN OKORO <chinedu.godwin@ccsnl.com>
2021-09-04 23:49:45 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,81 +1,94 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
+
 import axios from "axios";
 
-class App extends Component {
-  constructor(props){
-    super(props)
-    this.state={
-      playerName: null,
-      playerStats: {}
+const App = () => {
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     playerName: null,
+  //     playerStats: {}
+  //   }
+  // }
+  const [playerName, setPlayerName] = useState(null)
+  const [playerStats, setPlayerStats] = useState('')
+  const [name, setName] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getPlayerId()
+    console.log(playerName)
+  }
+
+  const handleChange = (event) => {
+    const replace = event.target.value.split(" ").join("_");
+    if (replace.length > 0) {
+      setPlayerName(replace)
+    } else {
+      alert("Please type players name!")
     }
   }
 
-handleSubmit = (e) => {
-  e.preventDefault();
-  this.getPlayerId()
-  console.log(this.state.playerName)
-}
-
-handleChange = (event) => {
-  const replace = event.target.value.split(" ").join("_");
-  if(replace.length > 0){
-    this.setState({playerName: replace})
-  } else {
-    alert("Please type players name!")
-  }
-}
-
-  getPlayerId = () => {
-    axios.get(`https://www.balldontlie.io/api/v1/players?search=${this.state.playerName}`)
-    .then(async res => {
-      // console.log(res.data.data)
-      if(res.data.data[0] === undefined){
-        alert("This player is either injured or hasn't played yet!")
-      } else if(res.data.data.length > 1){
-        alert("Pleases specify the name more!")
-      } else{
-        await this.getPlayerStats(res.data.data[0].id)
-
-      }
-    }).catch(err => {
-      console.log(err)
-    })
+  const getPlayerId = () => {
+    axios.get(`https://www.balldontlie.io/api/v1/players?search=${playerName}`)
+      .then(async res => {
+        console.log(res.data.data)
+        setPlayerStats(res.data.data)
+        if (res.data.data[0] === undefined) {
+          alert("This player is either injured or hasn't played yet!")
+        } else if (res.data.data.length > 1) {
+          alert("Pleases specify the name more!")
+        } else {
+          await setName(res.data.data[0].id)
+          setName(res.data.data)
+          console.log(res.data.data[0].id)
+        }
+      }).catch(err => {
+        console.log(err)
+      })
   }
 
-  getPlayerStats = (playerId) => {
+  console.table(playerStats)
+
+  const getPlayerStats = (playerId) => {
     axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=2006&player_ids[]=${playerId}`)
-    .then(async res => {
-      console.log(res.data.data)
-      this.setState({ playerStats: res.data.data[0]})
-    }).catch(err => {
-      console.log(err)
-    })
+      .then(async res => {
+        console.log(res.data.data)
+
+        setPlayerStats(res.data.data)
+      }).catch(err => {
+        console.log(err)
+        getPlayerStats()
+      })
   }
-  
-  render(){
+
+
+
+
   return (
     <div className="App">
-     <form onSubmit={this.handleSubmit}>
-       <label>
-         Name
-         <input 
-          type="text"
-          value={this.state.value}
-          onChange={this.handleChange}
-          placeholder="please enter players name"
-         />
-       </label>
-       <input type="submit" value="Submit"/>
-     </form>
-     games played: {this.state.playerStats["games_played"]}
-     <br />
-     points averaged: {this.state.playerStats["pts"]}
-     <br />
-     rebounds averaged: {this.state.playerStats["reb"]}
-     <br />
-     assists averaged: {this.state.playerStats["ast"]}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name
+          <input
+            type="text"
+            // value={event}
+            onChange={handleChange}
+            placeholder="please enter players name"
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      games played: {playerStats.first_name}
+      <br />
+      points averaged: {playerStats["pts"]}
+      <br />
+      rebounds averaged: {playerStats["reb"]}
+      <br />
+      assists averaged: {playerStats["ast"]}
+
     </div>
   );
-}
+
 }
 export default App;
